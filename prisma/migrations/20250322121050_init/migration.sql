fbeb5cfb5a6e2c534da43ceddf2a3ccb9f5a7542
Asfar Jamil <asfarj@withnubo.com>
2025-03-26 03:47:09 -0400

    feat: postgresql db using Neon

--- prisma/migrations/20250322121050_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_id_key" ON "User"("id");

70ddc923efdfa13dbc1830f3ac6d97588f768fe6
Asfar Jamil <asfarj@withnubo.com>
2025-03-27 14:36:56 -0400

    chore: test env var

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,8 @@
 import type { Load } from './$types';
 
 export const load: Load = async ({ locals, url, cookies }) => {
-    console.log(process.env.TEST as string)
+    const test = process.env.TEST as string
     cookies.delete("userId", { path: "/" });
+    return ({test})
 }
 

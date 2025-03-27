b82dee814a648b5ffee421e012b210fb2092d741
Asfar Jamil <asfarj@withnubo.com>
2025-03-27 14:49:01 -0400

    chore: undid test env

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,6 @@
 import type { Load } from './$types';
 
 export const load: Load = async ({ locals, url, cookies }) => {
-    const test = process.env.TEST as string
     cookies.delete("userId", { path: "/" });
-    return ({test})
 }
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
-<script lang="ts">
-    let { data } = $props();
-    let test = data.test
-    console.log(test)
-</script>
-
 <div class=" w-full h-screen bg-black place-content-center text-center max-h-screen overflow-hidden">
     <div class="absolute left-0 right-0 top-0 bottom-0 h-screen w-screen z-50" >
         <button onclick={ () => window.location.href = '/auth'} class="cursor-pointer hover:bg-neutral-300 hover:text-black transition-all h-fit max-w-32 mx-auto my-auto absolute left-0 right-0 top-0 bottom-0 border rounded-full py-4 px-2 text-xl border border-neutral-500 bg-neutral-950 text-neutral-300">
